fbaaaacd752aa4ce4a1a3b7369c7f222a08ea0dc
EC2 Default User <ec2-user@ip-172-31-84-145.ec2.internal>
2022-07-05 15:41:37 +0000

    second commit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,11 +1,16 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item=Item.new
   end
 
   def index
+    @items = Item.all
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
   end
 
   def show
@@ -15,5 +20,11 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  has_one_attached:image
+  belongs_to :item
+  
+  
 end
